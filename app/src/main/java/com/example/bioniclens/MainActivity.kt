4a52a4b723a3f35af2d3eec450d0b8cfd3ecba28
Sebastian <sebi.atletico99@yahoo.com>
2021-10-25 23:51:47 +0300

    Start camera activity

--- app/src/main/java/com/example/bioniclens/MainActivity.kt
+++ app/src/main/java/com/example/bioniclens/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.bioniclens
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraExecutor: ExecutorService;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cameraButton = findViewById<Button>(R.id.cameraButton)
+
+        cameraButton.setOnClickListener{
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
